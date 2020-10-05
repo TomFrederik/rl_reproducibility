9a57c3f0e4ef26efbedb35bef463e4d6b8e9c0df
Erik Jenner <erik.jenner99@gmail.com>
2020-10-05 10:18:51 +0200

    Small changes to Experiment class

--- experiment_class.py
+++ experiment_class.py
@@ -6,16 +6,17 @@ import algorithms
 
 class Experiment:
 
-    def __init__(self, seed, env_str, ac, ac_kwargs, ac_alg, ac_alg_kwargs, v=None, v_kwargs=None, v_alg=None, v_alg_kwargs=None, num_iters=100, ep_per_iter=5):
+    def __init__(self, seed, env_str, ac, ac_kwargs, ac_alg, ac_alg_kwargs, target_alg, target_alg_kwargs, v=None, v_kwargs=None, v_alg=None, v_alg_kwargs=None, num_iters=100, ep_per_iter=5):
         '''
         Input:
             seed - random seed for the experiment
             env_str - string to describe a gym environment
-            ac - an actor object
+            ac - an Actor subclass
             ac_kwargs - dict, kwargs for ac
-            train_alg - training algorithm class, should be NPG or TRPO
+            ac_alg - ActorAlgorithm subclass, should be NPG or TRPO
             alg_kwargs - dict, kwargs for the training algorithm, contains hyperparams
-            v - a critic object
+            target_alg - TargetAlgorithm subclass
+            v - a Critic class
             v_kwargs - dict, kwargs for v
             num_iters - int, number of iterations / updates applied to the policy
             ep_per_iter - int, number of episodes to use for updating per iteration
@@ -31,9 +32,10 @@ class Experiment:
         self.env.seed(self.seed)
 
         self.actor = ac(env=self.env, **ac_kwargs)
-        self.ac_alg = ac_alg(**ac_alg_kwargs)
+        self.target_alg = target_alg(**target_alg_kwargs)
+        self.ac_alg = ac_alg(self.actor, self.target_alg, **ac_alg_kwargs)
 
-        if not (v == None):
+        if not (v is None):
             self.critic = v(**v_kwargs)
             self.v_alg = v_alg(**v_alg_kwargs)
             self.v.optim = torch.optim.Adam(self.critic.parameters(), lr=1e-3) # should this be in one of the kwargs, or passed via v_lr?
