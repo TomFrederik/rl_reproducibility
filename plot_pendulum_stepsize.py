8541578d0f51056677b4a90b67c39b92744dd13e
ruihan0495 <34216537+ruihan0495@users.noreply.github.com>
2020-10-22 16:46:24 +0200

    add return plot

--- plot_pendulum_stepsize.py
+++ plot_pendulum_stepsize.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-data_dir = './kl/logs/Pendulum-v0/'
-plot_dir = './kl/plots/Pendulum-v0/'
+data_dir = './kl/logs/Acrobot-v1/'
+plot_dir = './kl/plots/Acrobot-1/'
 
 trpo_files = [data_dir + 'TRPO/' + str(seed) + '_log.npz' for seed in range(10,15)]
 npg_files = [data_dir + 'NPG/' + str(seed) + '_log.npz' for seed in range(10,15)]
@@ -12,12 +12,22 @@ max_kl = 0.01
 
 # load step_size data
 trpo_step_sizes = []
+trpo_returns = []
+npg_returns = []
 
 for i, f in enumerate(trpo_files):
-    data = np.load(f)['step_size']
-    trpo_step_sizes.append(data)
+    data_step_size = np.load(f)['step_size']
+    trpo_step_sizes.append(data_step_size)
+    data_returns = np.load(f)['returns']
+    trpo_returns.append(data_returns)
+
+for i, f in enumerate(npg_files):
+    data_returns = np.load(f)['returns']
+    npg_returns.append(data_returns)
 
 trpo_step_sizes = np.array(trpo_step_sizes)
+trpo_returns = np.array(trpo_returns)
+npg_returns = np.array(npg_returns)
 
 mean_stepsize = np.mean(trpo_step_sizes, axis=0)
 std_stepsize = np.std(trpo_step_sizes, axis=0)
@@ -30,4 +40,24 @@ plt.ylabel('Step Size')
 plt.xlabel('Iteration')
 plt.title('Max KL = 0.01')
 plt.legend()
+plt.show()
 plt.savefig(plot_dir + 'trpo_stepsize_{}.pdf'.format(max_kl))
+
+mean_trpo_return = np.mean(trpo_returns, axis=0)
+std_trpo_return = np.std(trpo_returns, axis=0)
+mean_npg_return = np.mean(npg_returns, axis=0)
+std_npg_return = np.std(npg_returns, axis=0)
+
+plt.figure()
+plt.plot(mean_trpo_return, label="TRPO")
+plt.plot(mean_npg_return, label="NPG")
+plt.fill_between(np.arange(len(mean_trpo_return)), mean_trpo_return - std_trpo_return, 
+                    mean_trpo_return + std_trpo_return, alpha=.4, label=r'$\pm 1 \sigma$')
+plt.fill_between(np.arange(len(mean_npg_return)), mean_npg_return - std_npg_return, 
+                    mean_npg_return + std_npg_return, alpha=.4, label=r'$\pm 1 \sigma$')
+plt.ylabel('Return')
+plt.xlabel('Iteration')
+plt.title('Average Return')
+plt.legend()
+plt.show()
+plt.savefig(plot_dir + 'avg_return.pdf')
